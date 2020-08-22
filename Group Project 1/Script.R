2b3bb1f9d1be98f1f4ce2ea28445c9a5494e821c
Jake Gadaleta <jake.gads@gmail.com>
2020-08-22 17:11:34 -0400

    Format for the group project 1

---
+++ Group Project 1/Script.R
@@ -0,0 +1,17 @@
+library(tidyverse) #loading the lib
+
+storms
+
+# name  |   year    |month  |day    |hour   |lat    
+# <chr> |   <dbl>   |<dbl>  |<int>  |<dbl>  |<dbl>  
+
+# |long   |status |category   |wind   |pressure
+# |<dbl>  |<chr>  |<ord>      |<int>  |<int>
+
+head = ggplot(data=storms)
+
+head + 
+geom_bar(mapping=aes(x=year))
+
+head +
+geom_bar(mapping=aes(x=name))
