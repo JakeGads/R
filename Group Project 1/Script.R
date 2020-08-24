d52efaad10bc50e93d74244380029b18d8edcef9
Jake Gadaleta <jake.gads@gmail.com>
2020-08-24 18:45:32 -0400

    finsihing up the project graphs

--- Group Project 1/Script.R
+++ Group Project 1/Script.R
@@ -10,57 +10,57 @@ storms
 
 head = ggplot(data=storms)
 
-# 0.0
+# 0.0 Playing Around
 head +
 geom_bar(mapping=aes(x=year)) +
 ggtitle("Years in the dataset")
 
-# 0.1
+# 0.1 Playing Around
 head +
 geom_bar(mapping=aes(x=name)) +
 ggtitle("Repeating names")
 
-# 1.0
+# 1.0 Jake
 head +  
 geom_point(mapping=aes(x=lat, y=long, color=wind))  +
 ggtitle("Lat v Long (Wind Edition)")
 
-# 1.1
+# 1.1 Jake
 head +  
 geom_point(mapping=aes(x=lat, y=long, color=pressure)) +
 ggtitle("Lat v Long (Pressure Edition)")
 
-# 1.2
+# 1.2 Jake
 head +  
 geom_point(mapping=aes(x=lat, y=long, color=category)) +
 ggtitle("lat v long (Category Edition)")
 
-# 2
+# 2 Jake
 head +  
 geom_point(mapping=aes(x=wind, y=pressure, color=category)) +
 geom_smooth(mapping = aes(x = wind, y = pressure)) +
 ggtitle("Wind v Presure w/ category")
 
-# 3
+# 3 Jake
 head +  
 geom_point(mapping=aes(x=month, y=year, color=hour)) +
 ggtitle("What time storms happen every year")
 # This probablly really says something cool if I could see colors
 
-# 4
+# 4 Cam
 head + 
-geom_point(mapping = aes(x = year, y = pressure)) +
+geom_point(mapping = aes(x = year, y = pressure), color="blue") +
 ggtitle("Year v Pressure")
 
-# 5
+# 5 Cam
 head + 
-geom_point(mapping = aes(x = month, y = pressure)) + 
+geom_point(mapping = aes(x = month, y = pressure), color="blue") + 
 ggtitle("Month v Pressure")
 # The pressure in the storms did not have a trend from year to year, but there was a trend across months. 
 # Typically, summer months had a much higher spread in pressure than winter months.
 
 
-# 6
+# 6 Cole
 head + 
 geom_bar(mapping=aes(x=status, fill=status)) + 
 facet_wrap(~year) +
--- Group Project 1/tempCodeRunnerFile.R
+++ Group Project 1/tempCodeRunnerFile.R
@@ -1,5 +1,2 @@
-# 6
-head + 
-geom_bar(mapping=aes(x=status, fill=status)) + 
-facet_wrap(~year) +
-ggtitle("Status per year")
+
+# 1.1 Jake
