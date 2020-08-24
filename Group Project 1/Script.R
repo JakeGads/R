431fd39486398314bc769813ec1a175cf185e6e3
Jake Gadaleta <jake.gads@gmail.com>
2020-08-24 09:55:52 -0400

    added another graph

--- Group Project 1/Script.R
+++ Group Project 1/Script.R
@@ -10,8 +10,30 @@ storms
 
 head = ggplot(data=storms)
 
+# These 2 are examples the idea is that we see the spread of years alongside the names
 head + 
 geom_bar(mapping=aes(x=year))
 
 head +
 geom_bar(mapping=aes(x=name))
+
+head +  
+geom_point(mapping=aes(x=month, y=year, color=hour)) 
+# This probablly really says something cool if I could see colors
+
+# same graph different colors 0
+head +  
+geom_point(mapping=aes(x=lat, y=long, color=wind)) 
+
+# same graph different colors 1
+head +  
+geom_point(mapping=aes(x=lat, y=long, color=pressure)) 
+
+# same graph different colors 2
+head +  
+geom_point(mapping=aes(x=lat, y=long, color=category)) 
+
+
+head +  
+geom_point(mapping=aes(x=wind, y=pressure, color=category)) +
+geom_smooth(mapping = aes(x = wind, y = pressure)) 
