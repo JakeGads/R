5f8b99c4722e7829d6df6acb9b53c0283579c1fc
Jake Gadaleta <jake.gads@gmail.com>
2020-08-27 10:22:35 -0400

    Syncing

--- Group Projects/Group Project 1/tempCodeRunnerFile.R
+++ Group Projects/Group Project 1/tempCodeRunnerFile.R
@@ -1,2 +1,5 @@
-
-# 1.1 Jake
+# 3 Jake
+head +  
+geom_point(mapping=aes(x=month, y=year, color=hour)) +
+ggtitle("What time storms happen every year [Jake]")
+# This probablly really says something cool if I could see colors
