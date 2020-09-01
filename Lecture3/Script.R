7dd1e66ee07ac52459b9529ee7278f9df114bee0
Jake Gadaleta <jake.gads@gmail.com>
2020-09-01 09:55:35 -0400

    Class stuff from 9/1

---
+++ Lecture3/Script.R
@@ -0,0 +1,36 @@
+# Block 2
+# playing with pictures and data
+
+# install.packages("nycflights13")
+# install.packages("tidyverse")
+
+library(nycflights13)
+library(tidyverse)
+flights
+
+head = ggplot(flights)
+
+# God I hate being color blind
+head + geom_point(map=aws(dep_delay, arr_delay, color=carrier))
+
+head + 
+geom_point(map=aws(dep_delay, arr_delay, color=carrier)) +
+facet_wrap(~month)
+
+# Might find there is a month or 2 that are particularily interesting
+# Focus on one of the month we have decied that to be June
+
+# Transformation Methods
+
+# *transform*(data, specifics) -> generate a new dataset
+
+# 1. filter -> pull out only opservations with specific values
+head <- ggplot(filter(flights, month==1))
+
+head +
+geom_point(map=aws(dep_delay, arr_delay, color=carrier)) +
+facet_wrap(~day)
+
+# we can expand on a filter
+head <- ggplot(filter(flights, month==1, day==13))
+
