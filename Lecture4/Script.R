6cac2a8aa3c5b4b90853ed3fbc17a68725993841
Jake Gadaleta <jake.gads@gmail.com>
2020-09-03 10:27:31 -0400

    Added lecture 4

---
+++ Lecture4/Script.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(nycflights13)
+flights
+filter(flights, dep_time<=600 | dep_time>=1800)
+
+head <- ggplot(flights)
+
+head + geom_point(mapping=aes(dep_time, dep_delay))
+
+head + geom_point(mapping=aes(dep_time, dep_delay, color=carrier))
+
+a <- filter(flights, dep_time > 500 & dep_time < 1500, dep_delay > 500) 
+
+ggplot(a) + 
+geom_point(mapping=aes(dep_time, dep_delay))
+
+arrange(a, desc(dep_delay))
+
+a
+
+a <- select(a, month, day, dep_time, dep_delay)
+
+arrange(a, desc(month), desc(dep_delay))
+
+select(flights, contains("delay"))
+
+select(flights, year:day)
+
+flights
+
+ggplot(flights) +
+geom_point(mapping=aes(tailnum, arr_delay))
+
+a <- filter(flights, arr_delay > 500)
+
+ggplot(a) +
+  geom_point(mapping=aes(tailnum, arr_delay, color=carrier))
+
+arrange(a, carrier)
+a <- select(a, arr_delay, tailnum, carrier)
+a
+
+ggplot(a) +
+  geom_point(mapping=aes(carrier, arr_delay, color=carrier))
+
+
+a <- mutate(flights, gain=dep_delay-arr_delay)
+a <- select(flights, month, day, dep_delay, arr_delay)
+a <- mutate(a, gain = dep_delay, arr_delay)
+
+transmute(flights, gain=dep_delay - arr_delay)
+
+# modulus %/%
+
+mutate(a, next_delay=lead(dep_delay))
+mutate(a, last_delay=lag(dep_delay))
+
+a <- mutate(a, how_bad=min_rank(dep_delay))
+
+arrange(a, how_bad)
+
+filter(a, how_bad >= 100000 &how_bad<=200000)
+
+filter(select(flights, month:day, contains("delay")), dep_delay<0)
+
+a <- flights %>% 
+  select(month:day, contains("delay")) %>% 
+  filter(dep_delay<0) %>%
+  mutate(gain= dep_delay-arr_delay) %>%
+  filter(gain<60)
+# Read as  Then
+
+summarise(a, mean(gain), na.rm=TRUE)
