0e798c4ada1c34732696cae63e73c58598735296
Jake Gadaleta <jake.gads@gmail.com>
2020-09-06 23:51:56 -0400

    wrapped up my second graph

--- Group Projects/Group Project 2/Script.R
+++ Group Projects/Group Project 2/Script.R
@@ -46,3 +46,32 @@ ggplot(midwest) +
 geom_point(mapping=aes(popadults, poppovertyknown, color=popdensity)) +
 geom_smooth(mapping=aes(popadults, poppovertyknown)) +
 gg_title("Pop Density w/ regression line")
+
+# This really brings us to the shocking conclusion that the more people exist in a place the higher amount of poverty as well.
+
+# 2 Jake
+
+
+ggplot(midwest) +
+geom_point(aes(popdensity, percbelowpoverty))
+
+ggplot(midwest) +
+geom_point(aes(popdensity, percbelowpoverty, color=inmetro == 1))
+
+# Take note of really high non metro ones
+
+j2 <- midwest %>%
+filter(inmetro == 1)
+
+ggplot(j2) +
+geom_point(aes(popdensity, percbelowpoverty, color=poppovertyknown))
+
+# clearing out some of the bottom values
+
+j2 <- midwest %>%
+filter(inmetro == 1) %>%
+filter(percbelowpoverty > 12.5)
+
+ggplot(j2) +
+geom_point(aes(popdensity, percbelowpoverty, color=percollege))
+# now we can see the percentage below poverty, really isn't impacted by population density or percentage that attended college
