c811faa7e7a706b4d76d798a21242af45839e4ed
Jake Gadaleta <jake.gads@gmail.com>
2020-09-18 12:10:51 -0400

    added import states and col name generators

--- Group Projects/Group Project 3/Jake.R
+++ Group Projects/Group Project 3/Jake.R
@@ -16,3 +16,12 @@ theme_set(theme_minimal())
 options(ggplot2.continuous.colour="viridis")
 options(ggplot2.continuous.fill = "viridis")
 #endregion
+
+getwd() # for error checking
+airlines <- read_csv("airlines.csv")
+drivers <- read_csv("drivers.csv")
+states <- read_csv("states.csv")
+
+colnames(airlines)
+colnames(drivers)
+colnames(states)
