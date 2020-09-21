2693b2a4ac4b46a27109f362b2a929aca59b0bfd
Jake Gadaleta <jake.gads@gmail.com>
2020-09-21 17:19:52 -0400

    Syncing

--- Group Projects/Group Project 3/Script.R
+++ Group Projects/Group Project 3/Script.R
@@ -20,7 +20,13 @@ options(ggplot2.continuous.fill = "viridis")
 setwd('~/source/repo/R/Group Projects/Group Project 3') # For Jake
 
 getwd() # for error checking
+
+#region Data Parsing
+
+# Loading in data
+# Load the data into R from the given csv file. In this step, you must make sure every column of data is appropriately labeled and has the correct data type associated. This must be done every column, even if you do not plan on using a specific column for your subsequent analysis.
 # no skips but col names need to be overridden
+
 airlines <- read_csv("airlines.csv")
 
 drivers <- read_csv(
@@ -54,11 +60,50 @@ states <- read_csv(
 select(-error_catch)
 
 
-megastate <- merge(drivers, states, by="state") %>% # combines drivers & states
-subset(pop_18 != NA & num_drivers_in_fatal_per_billion != NA) # drops columns that lack the overlap
-
+megastate <- merge(drivers, states, by="state")
 colnames(airlines)
 colnames(drivers)
 colnames(states) 
 colnames(megastate)
 
+#endregion
+
+#region Airlines 1 (Moses)
+
+# total_fatalites X total_incidents
+
+#endregion
+
+#region Airlines 2 (Cam)
+
+# something that changes per year
+
+#endregion
+
+#region Drivers 1 (Jake)
+
+# "num_drivers_in_fatal_per_billion" x "insurance_company_losses_per_driver"
+
+ggplot(data = drivers, aes(num_drivers_in_fatal_per_billion, insurance_company_losses_per_driver)) +
+geom_point()
+
+#endregion
+
+
+#region Drivers 2 (Moses)
+
+# Districted x per_not_distracted
+
+#endregion
+
+#region States 1 (Cam)
+
+# pop_per_square_mile x income
+
+#endregion
+
+#region States 2 (Jake)
+
+# Travel_time vs income
+
+#endregion
