0936f30e0d6ae08a29431638f118f4eb3b4b6c25
Jake Gadaleta <jake.gads@gmail.com>
2020-10-06 22:33:02 -0400

    wrapped up

--- Group Projects/Group Project 4/Script.R
+++ Group Projects/Group Project 4/Script.R
@@ -51,7 +51,73 @@ pivot_longer(
 )
 colnames(pop)
 
-# we must always <some_kind>_join the smaller dataset with the larger one
+#Andrew Chu
+#code 1
+a1 <- water %>% inner_join(life) %>% inner_join(energy)
+
+ggplot(data = a1) + geom_point(mapping = aes(x = life_expectancy, y = energy))
+
+a1 <- arrange(a1, life_expectancy) %>% filter(life_expectancy > 80)
+
+ggplot(data = a1) + geom_point(mapping = aes(x = life_expectancy, y = energy), color = "blue") + geom_point(mapping = aes(x = life_expectancy, y = per_water_usage), color = "red")
+#can't tell anything about water usage so it needs to be separated
+
+ggplot(data = a1) + geom_point(mapping = aes(x = life_expectancy, y = energy), color = "blue")
+
+a1 <- filter(a1, energy < 15000)
+
+ggplot(data = a1) + geom_point(mapping = aes(x = life_expectancy, y = per_water_usage, color = country))
+ggplot(data = a1) + geom_point(mapping = aes(x = life_expectancy, y = energy, color = country))
+
+#conclusion: high life expectency seems to correlate with large access to drinking water and there seems to be no correlation with energy usage.
+
+
+#code 2
+b1 <- water %>% inner_join(life) %>% inner_join(literacy)
+
+ggplot(data = b1) + geom_point(mapping = aes(x = life_expectancy, y = per_water_usage))
+#large spread around the lower life expectancy. Correlations?
+
+b1 <- filter(b1, life_expectancy < 45)
+#dying by 45 is too young, maybe we can help identify some problems?
+
+ggplot(data = b1) + geom_point(mapping = aes(x = life_expectancy, y = per_water_usage, color = country))
+#clearly very low water supplies in these countries
+
+ggplot(data = b1) + geom_point(mapping = aes(x = life_expectancy, y = lit_rate, color = country))
+#unfortunately, not all countries have a literacy rate. Possibly because it is diificult enough to survive on a day to day basis in these countries.
+
+ggplot(data = b1) + geom_smooth(mapping = aes(x = per_water_usage, y = life_expectancy))
+#unusual dip in the graph. Why?
+
+b1 <- filter(b1, life_expectancy < 35)
+
+ggplot(data = b1) + geom_point(mapping = aes(x = per_water_usage, y = life_expectancy, color = country))
+#Haiti is a massive outlyer with about a 33 year life expectancy.
+
+#region Leandro
+#region 1
+
+#Average Income, Literacy Rate, and Population (Leandro Pares)
+inner_join(income,literacy) %>% na.omit(lit_rate)
+lp1<-inner_join(income,literacy) %>% na.omit(lit_rate)
+lp1A<-inner_join(lp1,pop)
+ggplot(data=lp1A) + geom_point(mapping=aes(avg_income,lit_rate, color=population))
+lp1B<-filter(lp1A,population < 1.00e+08)
+ggplot(data=lp1B) + geom_point(mapping=aes(avg_income,lit_rate, color=population))
+ggplot(data=lp1B) + geom_point(mapping=aes(avg_income,lit_rate, color=population)) + facet_wrap(~year)
+ggplot(data=lp1B) + geom_point(mapping=aes(avg_income,lit_rate, color=population)) + facet_wrap(~year) + labs(title="Average Income Compared to Literacy Rate", x="Average Income", y="Literacy Rate", color="Population")
+#endregion
+
+#region 2
+#Average Income, Water Access, and Life Expectancy (Leandro Pares)
+lp2<-inner_join(income, water)
+lp2A<-inner_join(lp2,life)
+ggplot(data=lp2A) + geom_point(mapping=aes(per_water_usage,life_expectancy,color=avg_income))
+ggplot(data=lp2A) + geom_point(mapping=aes(per_water_usage,life_expectancy,color=avg_income)) + facet_wrap(~year)
+ggplot(data=lp2A) + geom_point(mapping=aes(per_water_usage,life_expectancy,color=avg_income)) + facet_wrap(~year) + labs(title="Water Usage Compared to Life Expectancy", x="Water",y="Life Expectancy",color="Mean Income")
+#endregion
+#endregion
 
 #region Jake
 #region 1
@@ -111,7 +177,6 @@ theme(
 
 j2 <- income %>%
 group_by(country) %>%
-summarize(avg_income_ = mean(avg_income)) %>%
 inner_join(energy) %>%
 inner_join(water) %>%
 na.omit(energy) %>%
