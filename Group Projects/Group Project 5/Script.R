dcc31a0c64a1faaadb81148b59800ecb36439528
Jake <jake.gads@gmail.com>
2020-10-16 14:12:49 -0400

    tried to make it more modular (unTested)

--- Group Projects/Group Project 5/Script.R
+++ Group Projects/Group Project 5/Script.R
@@ -4,8 +4,7 @@ library(tidyverse)
 clean_data <- function(csv_file) {
     # country, a bunch of years
     
-    gen_data <- 0
-    gen_data <- read_csv(paste(csv_file, '.csv', sep = ""))
+    gen_data <- read_csv(paste(csv_file, '.csv', sep = "")) # add .csv so the file is findable (this is called after the remove_csv function)
     
     r <- colnames(gen_data)
     
@@ -21,17 +20,16 @@ clean_data <- function(csv_file) {
 
 
 # joins the data when accepted as a string
-jake_join <- function(x,y,z=FALSE){
-    gen_data <- clean_data(x) %>%
-    inner_join(clean_data(y)) %>%
-    na.omit(x) %>%
-    na.omit(y)
+jake_join <- function(...){ # this means it can take an unlimited amount of
+    my_list <- list(...)
+    
+    gen_data <- clean_data(my_list[1]) %>%
+    na.omit(my_list[1])
 
-    if(z != FALSE){
-        
+    for (i in 2:length(my_list)){
         gen_data <- gen_data %>%
-        inner_join(clean_data(z)) %>%
-        na.omit(z)
+        inner_join(my_list(i)) %>%
+        na.omit(my_list[i])
     }
     
     return(gen_data)
@@ -47,37 +45,35 @@ remove_csv <- function(file){
 }
 
 # makes a scatterplot, either colored or uncolored
-generate_scatterplot <- function(x,y, c='None'){
-    x <- remove_csv(x)
-    y <- remove_csv(y)
-    c <- remove_csv(c)
+generate_scatterplot <- function(...){
+    my_list <- list(...)
+    
+    for (i in my_list){
+        my_list[i] <- remove_csv(my_list[i])
+    }
 
-    joined <- jake_join(x,y,c)
+    joined <- jake_join(...)
 
-    graph <- 0
-    if(c != 'None'){
-        graph <- joined %>%
-        ggplot(aes_string(x, y, color=c)) + # aes string allows for the column name to as a string and not a datapoint
-        geom_point() +
-        labs(
-            title=paste(x, 'vs', y, sep = " "),
-            subtitle=paste("colored by", c),
-            x=x,
-            y=y,
-            color=c
-        )
+    plot_call <- 0
+
+    if(my_list[3] != N/A){
+        plot_call <- ggplot(aes_string(my_list[1], my_list[2], my_list[1])) + # aes string allows for the column name to as a string and not a datapoint
+        
     }
     else{
-        graph <- jake_join(x,y) %>%
-        ggplot(mapping=aes_string(x,y)) + 
-        geom_point() +
-        labs(
-            title=paste(x, 'vs', y, sep = " "),
-            x=x,
-            y=y
-        )
+        plot_call <- ggplot(mapping=aes_string(x,y)) + 
     }
     
+    graph <- ggplot(joined) +
+    plot_call +
+    geom_point() +
+    labs(
+        title=paste(x, 'vs', y, sep = " "),
+        x=my_list[1],
+        y=my_list[2],
+        color=my_list[3]
+    )
+
     return(graph)
 }
 
