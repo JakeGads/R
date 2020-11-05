5c213739a81e05d69741ef7478dd9d220e463eaa
Jake Gadaleta <jake.gads@gmail.com>
2020-11-05 00:30:04 -0500

    expanded

--- Group Projects/Group Project 6/Script.R
+++ Group Projects/Group Project 6/Script.R
@@ -377,21 +377,29 @@ ccj_main_wrapper = function(graph_type=2, summarize_type=1, file_path="", locs=0
     )
 }
 
-print(cat(
-    "
-    USAGE:
-        Function Name: ccj_main_wrapper
+ccj_explain <- function() {
+    cat(
+        "
+        USAGE:
+            Function Name: ccj_main_wrapper
+            
+                Param 1 graph_type, default value 2: 
+                    1 for bar, 2 for scatter, 3 for density, 4 for line
+
+                Param 2 summarize_type, default 1:
+                    1 group mean, 2 individual mean, 3 sum(group zscores), 4 sum(indivual group zscores)
+
+                Param 3 file_path, default current directory:
+                    pass a string of the directory that the files exist in NOTE: it will also search sub direcotries for csv files.
+
+                Param 4 locs, default none:
+                    in alphabetical order which files you want to draw for your graph, the size will decided weither or not faceting is used and to what  
         
-            Param 1 graph_type, default value 2: 
-                1 for bar, 2 for scatter, 3 for density, 4 for line
+        
+        Example:
+            ccj_main_wrapper(graph_type=2, summarize_type=1, file_path=\"\", locs=0)
+        "
+    )
+}
 
-            Param 2 summarize_type, default 1:
-                1 group mean, 2 individual mean, 3 sum(group zscores), 4 sum(indivual group zscores)
-
-            Param 3 file_path, default current directory:
-                pass a string of the directory that the files exist in NOTE: it will also search sub direcotries for csv files.
-
-            Param 4 locs, default none:
-                in alphabetical order which files you want to draw for your graph, the size will decided weither or not faceting is used and to what             
-    "
-))
+ccj_explain()
